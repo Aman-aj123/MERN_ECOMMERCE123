651aed7fda7c2f56992a45fe0a232ecfda0e71b7
Aman-aj123 <amanraj123922@gmail.com>
2024-04-13 14:01:48 +0530

    Products banner is completed

--- frontend/src/Components/main/TopProducts/TopProducts.js
+++ frontend/src/Components/main/TopProducts/TopProducts.js
@@ -1,19 +1,145 @@
 import "./TopProducts.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight, faArrowsLeftRight } from "@fortawesome/free-solid-svg-icons";
+
 const TopProducts = () => {
     return (
         <div className="Top-Products">
+            <div className="slider-buttons primary-width">
+                <div className="slider-icn-wrapper flex">
+                    <FontAwesomeIcon icon={faArrowLeft} className="slider-btn" />
+                    <FontAwesomeIcon icon={faArrowRight} className="slider-btn" />
+                </div>
+            </div>
             <div className="Top-Products-wrapper mx-auto primary-width flex">
                 <div className="Top-Products-Items">
-                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg"/>
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2 className="w-full">Women's Fancy Tshirt's With Bag New</h2>
+                    <h3 className="w-full">Rs 899</h3>
+                    <p className="w-full">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
                     <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
                 </div>
                 <div className="Top-Products-Items">
-                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg"/>
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
                     <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
                 </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+                <div className="Top-Products-Items">
+                    <img className="w-full product-img" src="https://m.media-amazon.com/images/I/814hp8QMsjL._AC_SY200_.jpg" />
+                    <h2>Women's Fancy Tshirt's With Bag New</h2>
+                    <h3>Rs 899</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, delectus.</p>
+                </div>
+
             </div>
         </div>
     );
