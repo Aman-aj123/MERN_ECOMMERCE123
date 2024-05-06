f2ac546429c5799dd7caa81b737547c774986848
Aman-aj123 <amanraj123922@gmail.com>
2024-05-06 15:33:08 +0530

    top products is completed

--- frontend/src/Components/main/TopProducts/TopProducts.js
+++ frontend/src/Components/main/TopProducts/TopProducts.js
@@ -14,14 +14,19 @@ const TopProducts = () => {
 
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(1); 
     const limit = process.env.REACT_APP_API_LIMIT;
-    const URL = `${process.env.REACT_APP_API_BASE_URL}/api/products?page=page&limit=limit`;
+    const URL = `${process.env.REACT_APP_API_BASE_URL}/api/products?page=page&limit=${limit}`;
 
-    console.log(URL);
+    const limitArr = [];
+
+    for (let i = 0; i <= 10; i++) {
+        limitArr.push(i);
+    };
+
     const fetchData = async () => {
         const response = await FetchProducts(URL);
-        setProducts(response);
+        setProducts(response); 
         setLoading(false);
     };
 
@@ -34,37 +39,53 @@ const TopProducts = () => {
 
     const handlePrev = () => {
         setPage(page - 1);
+        console.log(page)
     };
 
     const handleNext = () => {
         setPage(page + 1);
+        console.log(page)
     };
+
+
+
 
     return (
         <div className="Top-Products">
             <div className="slider-buttons primary-width">
                 <div className="slider-icn-wrapper flex">
-                    <FontAwesomeIcon onClick={handlePrev} icon={faArrowLeft} className="slider-btn" />
-                    <FontAwesomeIcon onClick={handleNext} icon={faArrowRight} className="slider-btn" />
+                    <FontAwesomeIcon onClick={handlePrev} icon={faArrowLeft}   className={`slider-btn ${page <=1 ? "disabled-btn" : ""}`} />
+                    <FontAwesomeIcon onClick={handleNext} icon={faArrowRight}  className={`slider-btn ${page >= products?.response?.totalPages ? "disabled-btn" : ""}`} />
+                </div>
+                <div className="primary-width mx-auto">
+                    <p className="totalPages ">{page} of {products?.response?.totalPages}</p>
                 </div>
             </div>
+
             <div className="Top-Products-wrapper mx-auto primary-width flex">
-                {products?.response?.productItems.map((item) => (
-                    <div className="Top-Products-Items">
+                {products?.response?.productItems.map((item, index) => (
+                    <div key={index} className="Top-Products-Items">
                         <img className="w-full product-img" src={item?.images[0]} />
                         <h2 className="w-full product-title">{item?.title.slice(0, 30)}...</h2>
                         <div className="price-wrapper flex">
-                            <h3 className="w-full currentPrice">{item?.currentPrice}</h3>
-                            <h3 className="w-full oldPrice">{item?.oldPrice}</h3>
+                            <h3 className="w-full currentPrice">₹{item?.currentPrice}</h3>
+                            <h3 className="w-full oldPrice">₹{item?.oldPrice}</h3>
                         </div>
                         <p className="product-desc">{item?.description.slice(0, 40)}...</p>
-                        <h3 className="product-discount">{item?.discount}</h3>
+                        <h3 className="product-discount">{item?.discount}off</h3>
+
                     </div>
+
                 ))}
 
-                {loading && 
-                <h1>loading...</h1>
+                {loading &&
+                    <div className="loading-wrapper flex">
+                        {limitArr.map((element, index) => (
+                            <img key={index} className="loading-img" src="https://assets-v2.lottiefiles.com/a/53b80118-1161-11ee-b538-4f02e47c3050/EtQmNhvlO1.gif" />
+                        ))}
+                    </div>
                 }
+
             </div>
         </div>
     );
